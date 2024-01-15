b1d6290fd34708e3ed5c4a02e325ed75f6894321
SaiSanjit <saisanjit.d@gmail.com>
2024-01-15 23:11:19 +0530

    staterd employee model

---
+++ lib/views/models/employee_model.dart
@@ -0,0 +1,15 @@
+class EmployeeModel {
+  String email;
+  String username;
+  String employee_id;
+  String department;
+  String floor;
+
+  EmployeeModel(
+    this.email,
+    this.username,
+    this.employee_id,
+    this.department,
+    this.floor,
+  );
+}
